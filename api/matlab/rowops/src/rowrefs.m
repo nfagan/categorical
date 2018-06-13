d0a82f724eba556be9af6469b81d77710a712eeb
nfagan <nick@fagan.org>
2018-06-13 15:45:22 -0400

    add rowref

---
+++ api/matlab/rowops/src/rowrefs.m
@@ -0,0 +1,25 @@
+function d = rowrefs(data, I, un)
+
+%   ROWREFS -- Row-reference data for indexed subsets.
+%
+%     B = rowrefs( A, I ); calls `rowref` separately for each subset of
+%     rows of `A` identified by each index in `I`. `B` is a cell array
+%     whose elements are arrays of row-referenced subsets of `A`.
+%
+%     B = rowrefs( A, I, UNIFORM ) specifies whether the output of the
+%     indexing expression A(i, ...) is uniform across indices in `I`. If
+%     true, `B` is of the same class as `A`. Otherwise, `B` is a cell
+%     array. Default is false.
+%
+%     See also rowref
+%
+%     IN:
+%       - `data` (/T/)
+%       - `I` (cell array of index vectors)
+%     OUT:
+%       - `d` (cell array of /T/)
+
+if ( nargin < 3 ), un = false; end
+d = rowop( data, I, @(x) x, un );
+
+end
