fcb78448ff3390f6459ca0204c73003549d1483d
nfagan <nick@fagan.org>
2018-06-13 14:53:00 -0400

    fixed function pointer

--- api/matlab/rowops/src/rownanstd.cpp
+++ api/matlab/rowops/src/rownanstd.cpp
@@ -3,7 +3,7 @@
 
 namespace util {
 
-    void rowstd(double* in_data, size_t m_in, size_t n_in, 
+    void rownanstd(double* in_data, size_t m_in, size_t n_in, 
             double* out_data, size_t out_row, size_t out_rows, 
             uint64_t* indices, size_t n_indices)
     {      
@@ -65,5 +65,5 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     const char* func_id = "mean:main";
     
-    util::run(nlhs, plhs, nrhs, prhs, func_id, &util::rowstd);
+    util::run(nlhs, plhs, nrhs, prhs, func_id, &util::rownanstd);
 }
