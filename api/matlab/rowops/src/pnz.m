0fe154f10b69f48fdf727e93134d82164d72572e
nfagan <nick@fagan.org>
2018-07-05 14:23:05 -0400

    add pnz

---
+++ api/matlab/rowops/src/pnz.m
@@ -0,0 +1,17 @@
+function p = pnz(data)
+
+%   PNZ -- Percent non-zero elements.
+%
+%     p = pnz( DATA ); returns the fraction of the number of elements in 
+%     `DATA` that are non-zero.
+%
+%     See also nnz
+%
+%     IN:
+%       - `data` (/T/)
+%     OUT:
+%       - `p` (double)
+
+p = nnz( data ) / numel( data );
+
+end
