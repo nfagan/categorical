5451e08128211a13bd30bfe530e48816bb2269c9
nfagan <nick@fagan.org>
2018-07-15 19:09:25 -0400

    add space

--- api/matlab/cat_build.m
+++ api/matlab/cat_build.m
@@ -29,7 +29,7 @@ end
 if ( isunix() && ~ismac() )
   compiler_spec = 'GCC=''/usr/bin/gcc-4.9'' G++=''/usr/bin/g++-4.9''';
   cxx_std = 'c++1y';
-  addtl_c_flags = ' CFLAGS=''-fPIC''';
+  addtl_c_flags = ' CFLAGS=''-fPIC'' ';
   addtl_cxx_flags = '';
 else
   compiler_spec = '';
