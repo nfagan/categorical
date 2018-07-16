a79e9dcb763387cf850cd6a574c7601f33ad3a3d
nfagan <nick@fagan.org>
2018-07-16 13:09:58 -0400

    latest

--- api/matlab/cat_build.m
+++ api/matlab/cat_build.m
@@ -30,7 +30,7 @@ if ( isunix() && ~ismac() )
   compiler_spec = 'GCC=''/usr/bin/gcc-4.9'' G++=''/usr/bin/g++-4.9'' ';
 %   addtl_c_flags = ' CFLAGS="-fPIC" ';
   addtl_c_flags = '';
-  addtl_cxx_flags = 'CXXFLAGS="-std=c++1y"';
+  addtl_cxx_flags = 'CXXFLAGS="-std=c++1y -fPIC"';
 else
   compiler_spec = '';
   addtl_c_flags = '';
