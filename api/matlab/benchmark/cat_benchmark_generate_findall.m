7c0bf49a52d170f9ef8f84322a10a47a1f1c477b
nfagan <nick@fagan.org>
2019-11-04 11:43:29 -0500

    add additional benchmark

---
+++ api/matlab/benchmark/cat_benchmark_generate_findall.m
@@ -0,0 +1,18 @@
+function results = cat_benchmark_generate_findall()
+
+kinds = { 'small', 'large', 'large2' };
+
+results = cell( numel(kinds), 1 );
+
+for i = 1:numel(kinds)
+  f = fcat.example( kinds{i} );
+  [c, cats] = categorical( f );
+
+  params = cat_benchmark_config( 'iter', 1e3, 'tag', kinds{i} );
+  
+  results{i} = cat_benchmark_find( f, c, cats, params );
+end
+
+results = vertcat( results{:} );
+
+end
