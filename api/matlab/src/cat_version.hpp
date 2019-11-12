2f0e962a1f749dada5b4f1a5c86e2d50963563b7
nfagan <nick@fagan.org>
2019-11-12 14:01:54 -0500

    latest linux files

--- api/matlab/src/cat_version.hpp
+++ api/matlab/src/cat_version.hpp
@@ -1,3 +1,3 @@
-namespace util { const char* const CATEGORICAL_VERSION_ID = "3N7awgUas43IH3oCf3XSOMXkQmxGtod5"; }namespace util { namespace version { struct Version { static constexpr int major = 1;
+namespace util { const char* const CATEGORICAL_VERSION_ID = "y4H4nGRh77J87exIa4x7oC05quuYoKrB"; }namespace util { namespace version { struct Version { static constexpr int major = 1;
 static constexpr int minor = 0;
 static constexpr int patch = 1; }; } }
