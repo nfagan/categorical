6d97c482e9150bd016456e6d601ea4683c839178
nfagan <nick@fagan.org>
2019-11-14 16:32:59 -0500

    add util subdir

---
+++ api/matlab/util/cat_find_categorical.m
@@ -0,0 +1,40 @@
+function ind = cat_find_categorical(c, cats, selectors, f)
+
+%   CAT_FIND_CATEGORICAL -- fcat find equivalent for categorical.
+%
+%     ind = cat_find_categorical( c, cats, selectors, f ); produces a
+%     logical vector `ind` such that `isequal( find(ind), find(f,
+%     selectors)` is true. 
+%
+%     `c` is the categorical equivalent of `f`, and `cats` is the cell
+%     array of category names identifying columns of `c`. `selectors` is a
+%     cell array of strings or char.
+%
+%     See also cat_findall_categorical, fcat/find, fcat/fcat
+
+if ( ~iscell(selectors) )
+  selectors = cellstr( selectors );
+end
+
+selector_cats = whichcat( f, selectors );
+[groups, names] = findgroups( selector_cats );
+unique_groups = unique( groups );
+
+ind = true( size(c, 1), 1 );
+
+for i = 1:numel(unique_groups)
+  group_ind = find( groups == unique_groups(i) );
+  c_col = c(:, ismember(cats, names{i}));
+  
+  for j = 1:numel(group_ind)
+    if ( j == 1 )
+      res = c_col == selectors{group_ind(j)};
+    else
+      res = res | c_col == selectors{group_ind(j)};
+    end
+  end
+  
+  ind = ind & res;
+end
+
+end
--- api/matlab/util/cat_findall_categorical.m
+++ api/matlab/util/cat_findall_categorical.m
@@ -3,11 +3,7 @@ function [I, C] = cat_findall_categorical(categ)
 %   CAT_FINDALL_CATEGORICAL -- Produce output equivalent to fcat/findall,
 %     for a categorical matrix.
 %
-%     IN:
-%       - `categ` (categorical)
-%     OUT:
-%       - `I` (double)
-%       - `C` (categorical)
+%     See also cat_find_categorical
 
 [C, ~, ib] = unique( categ, 'rows' );
 I = accumarray( ib, (1:numel(ib))', [], @(rows) {sort(rows)} );
