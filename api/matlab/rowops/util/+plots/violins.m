a2b3964288dbe284a6655daf54a36972cf20497f
nfagan <nick@fagan.org>
2024-05-19 10:40:36 -0400

    specify panel shape

--- api/matlab/rowops/util/+plots/violins.m
+++ api/matlab/rowops/util/+plots/violins.m
@@ -1,11 +1,22 @@
-function axs = violins(data, I, id, L)
+function axs = violins(data, I, id, L, varargin)
 
 %   VIOLINS -- Grouped violin plots.
 %
 %     See also plots.panels, plots.simplest_barsets
 
+defaults = struct();
+defaults.panel_shape = [];
+
+params = shared_utils.general.parsestruct( defaults, varargin );
+
 [PI, PL] = plots.nest2( id, I, L );
-axs = plots.panels( numel(PI) );
+
+shape = numel( PI );
+if ( ~isempty(params.panel_shape) )
+  shape = params.panel_shape;
+end
+
+axs = plots.panels( shape );
 for i = 1:numel(PI)
   [g, v] = ungroupi( PI{i} );
   gl = PL{i, 2}(g);
